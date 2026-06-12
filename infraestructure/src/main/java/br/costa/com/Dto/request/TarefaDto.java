2d53163ad4b62193fced957707e53765542c5a3c
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-11 23:56:14 -0300

    feat(TarefaDto): completando a tarefaDto do infra

--- infraestructure/src/main/java/br/costa/com/Dto/request/TarefaDto.java
+++ infraestructure/src/main/java/br/costa/com/Dto/request/TarefaDto.java
@@ -1,7 +1,33 @@
 package br.costa.com.Dto.request;
 
+import br.costa.com.entity.tarefaEnum.TarefaEnum;
+
 public class TarefaDto {
     private String nome;
     private String descricao;
+    private TarefaEnum tarefaEnum;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public TarefaEnum getTarefaEnum() {
+        return tarefaEnum;
+    }
+
+    public void setTarefaEnum(TarefaEnum tarefaEnum) {
+        this.tarefaEnum = tarefaEnum;
+    }
 }
