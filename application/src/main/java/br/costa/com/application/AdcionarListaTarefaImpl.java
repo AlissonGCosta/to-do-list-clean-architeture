6d70bd21a90c6c3fa8438285e093ea86f238464d
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-12 21:11:53 -0300

    feat(AdcionarImpl): criando o metodo de adcionar na lista

---
+++ application/src/main/java/br/costa/com/application/AdcionarListaTarefaImpl.java
@@ -0,0 +1,29 @@
+package br.costa.com.application;
+
+import br.costa.com.entity.ListaTarefasEntity;
+import br.costa.com.entity.TarefaEntity;
+import br.costa.com.entity.tarefaEnum.TarefaEnum;
+import br.costa.com.useCase.AdcionarListaTarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdcionarListaTarefaImpl implements AdcionarListaTarefa {
+
+
+    List<ListaTarefasEntity> tarefas = new ArrayList<>();
+
+    @Override
+    public ListaTarefasEntity create(String nome, String descricao, TarefaEnum tarefaEnum) {
+
+        ListaTarefasEntity listaTarefa = new ListaTarefasEntity( nome, tarefaEnum, descricao);
+
+        this.tarefas.add(listaTarefa);
+
+       for(ListaTarefasEntity listaTarefaEntity : tarefas) {
+           System.out.println(listaTarefaEntity.getNome() + " - " + listaTarefaEntity.getDescricao() + " - " + listaTarefaEntity.
+                   getTarefaEnum());
+       }
+        return listaTarefa;
+    }
+}
