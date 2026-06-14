18eb405e47d4f6946e965565450c51ed4bb6fe99
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-14 13:57:04 -0300

    feat(TarefaMapper): Adcionando chamada o tarefaenum no import

--- infraestructure/src/main/java/br/costa/com/mapper/TarefaMapper.java
+++ infraestructure/src/main/java/br/costa/com/mapper/TarefaMapper.java
@@ -2,7 +2,7 @@ package br.costa.com.mapper;
 
 import br.costa.com.Dto.request.TarefaDto;
 import br.costa.com.entity.TarefaEntity;
-import br.costa.com.entity.tarefaEnum.TarefaEnum;
+
 
 public class TarefaMapper {
         public TarefaEntity toTarefaEntity(TarefaDto tarefaDto){
