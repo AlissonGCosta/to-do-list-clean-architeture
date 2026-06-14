d6bbddef449770a630d5aa9fe7819f57fea582fb
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-13 23:43:08 -0300

    feat(main): adcionando o menu de listar tarefas

--- infraestructure/src/main/java/br/costa/com/ToDoList.java
+++ infraestructure/src/main/java/br/costa/com/ToDoList.java
@@ -18,28 +18,65 @@ public class ToDoList {
 
         Scanner sc = new Scanner(System.in);
 
-        String continuar = "S";
+        boolean continuar = true;
 
-        while (continuar.equalsIgnoreCase("s")) {
+        while (continuar) {
 
-
-
-            System.out.println("Digite o nome do tarefa: ");
-            tarefaDto.setNome(sc.nextLine()) ;
-
-            System.out.println("Digite o descricao do tarefa: ");
-            tarefaDto.setDescricao(sc.nextLine());
-
-            createTarefa.create(tarefaMapper.toTarefaEntity(tarefaDto));
-
-            System.out.println("deseja continuar? (S/N)");
-            continuar = sc.next();
+            System.out.println("Escolha uma das opções abaixo: " +
+                    "\n 1 - Adcionar Tarefas" +
+                    "\n 2 - Listar tarefas ja Adcionadas" +
+                    "\n 0 - Sair");
+            int opcao = sc.nextInt();
             sc.nextLine();
 
-            if(continuar.equalsIgnoreCase("N")) {
-               continuar = "N";
+            switch (opcao) {
+
+                case 1:
+                    String continuarMenuAdcionar = "S";
+
+                    while (continuarMenuAdcionar.equalsIgnoreCase("s")) {
+
+
+
+                        System.out.println("Digite o nome do tarefa: ");
+                        tarefaDto.setNome(sc.nextLine()) ;
+
+                        System.out.println("Digite o descricao do tarefa: ");
+                        tarefaDto.setDescricao(sc.nextLine());
+
+                        createTarefa.create(tarefaMapper.toTarefaEntity(tarefaDto));
+
+                        System.out.println("deseja continuar? (S/N)");
+                        continuarMenuAdcionar = sc.next();
+                        sc.nextLine();
+
+                        if(continuarMenuAdcionar.equalsIgnoreCase("N")) {
+                            continuarMenuAdcionar = "N";
+                        }
+                    }
+                    break;
+
+                case 2:
+
+                    System.out.println("++++++++++++++++++++++++++++");
+                    System.out.println("Listando tarefas ja Adcionadas");
+                    System.out.println("++++++++++++++++++++++++++++");
+
+
+
+
+                    break;
+
+                case 0:
+                    System.out.println("Saindo do Programa");
+                    continuar = false;
+
+
+
             }
         }
+
+
 
 
     }
