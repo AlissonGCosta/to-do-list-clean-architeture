23dd1522df286833a7be18422e00c5e5ba332d20
Alisson G Costa <alisson.costa84200@gmail.com>
2026-06-14 14:00:12 -0300

    feat(CreateTarefa): mudando o nome para melhor legibilidade

--- usecase/src/main/java/br/costa/com/useCase/CreateTarefaUseCase.java
+++ usecase/src/main/java/br/costa/com/useCase/CreateTarefaUseCase.java
@@ -2,6 +2,6 @@ package br.costa.com.useCase;
 
 import br.costa.com.entity.TarefaEntity;
 
-public interface CreateTarefa {
+public interface CreateTarefaUseCase {
     TarefaEntity create(TarefaEntity tarefa);
 }
